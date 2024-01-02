fe49b0090632ea27d94fd9fac35520f7e825f28e
RRedBaron <nedelchevevg@gmail.com>
2024-01-02 17:38:23 +0200

    Fixed type errors in utils.ts

--- front/src/utils/filters.ts
+++ front/src/utils/filters.ts
@@ -1,20 +1,25 @@
-// filters.ts
+import {Booking} from '../types/booking'; // Замените на фактический путь к вашему файлу с типами
 
-import { Booking } from '../types/booking'; // Замените на фактический путь к вашему файлу с типами
+interface Filters {
+    createdAt?: 'asc' | 'desc';
+    reservedDate?: 'asc' | 'desc';
+    sessionType?: string;
+    price?: 'asc' | 'desc';
+}
 
-export const filterBookings = (bookings: Booking[], filters: any): Booking[] => {
-    let filtered = [...bookings];
+export const filterBookings = (bookings: Booking[], filters: Filters): Booking[] => {
+    let filtered: Booking[] = [...bookings];
 
     if (filters.createdAt === 'asc') {
-        filtered = filtered.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
+        filtered = filtered.sort((a, b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime());
     } else if (filters.createdAt === 'desc') {
-        filtered = filtered.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+        filtered = filtered.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
     }
 
     if (filters.reservedDate === 'asc') {
-        filtered = filtered.sort((a, b) => new Date(a.selectedDate) - new Date(b.selectedDate));
+        filtered = filtered.sort((a, b) => new Date(a.selectedDate).getTime() - new Date(b.selectedDate).getTime());
     } else if (filters.reservedDate === 'desc') {
-        filtered = filtered.sort((a, b) => new Date(b.selectedDate) - new Date(a.selectedDate));
+        filtered = filtered.sort((a, b) => new Date(b.selectedDate).getTime() - new Date(a.selectedDate).getTime());
     }
 
     if (filters.sessionType) {
